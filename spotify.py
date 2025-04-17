6267752c308e201c56524c9cfc9787a07251d04a
Corbyn Ledbetter <corbynledbetter@dynamic-10-125-202-0.frisco.wireless.unt.edu>
2025-04-17 17:33:11 -0500

    Testing my code with the recommendation system is working

--- spotify.py
+++ spotify.py
@@ -2,12 +2,26 @@
 # Client ID: fc3735a0571c4df6af0defd64b875c4b
 # Client Secret: 4ac6fac31b3441a3b7eeae3a9cc6d453
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-from collections import defaultdict
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
+env_file_path = 'ev.env'
+
+if os.path.exists(env_file_path):
+    with open(env_file_path) as f:
+        for line in f:
+            if line.strip() and not line.startswith("#"):
+                key, value = line.strip().split("=", 1)
+                os.environ[key] = value
+            else:
+                raise FileNotFoundError(f"{env_file_path} file not found")
 
 client_id = os.getenv('SPOTIPY_CLIENT_ID')
 client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
+redirct_uri = os.getenv('SPOTIPY_REDIRECT_URI')
 
 # Debugging the client_id's needed for the SpotifyClientCredentials to work
 print("SPOTIFY_CLIENT_ID:", os.getenv('SPOTIFY_CLIENT_ID'))
@@ -17,13 +31,29 @@ print("SPOTIFY_REDIRECT_URI:", os.getenv('SPOTIFY_REDIRECT_URI'))
 if not client_id or not client_secret:
     raise ValueError("Enviormental variables are not set")
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id=client_id,
-    client_secret=client_secret
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+    client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
+    redirect_uri=os.getenv('SPOTIPY_REDIRECT_URI'),
+    scope='user-top-read'
 ))
 
-results = sp.search(q='Imagine Dragons', limit=5)
+top_tracks = sp.current_user_top_tracks(limit=5)
+seed_tracks = [track['id'] for track in top_tracks['items']]
 
-print('API Response: ', results)
-for idx, track in enumerate(results['tracks']['items']): #
-    print(f"{idx + 1}. {track['name']} by {track['artists'][0]['name']}")
+top_artists = sp.current_user_top_artists(limit=5)
+seed_artists = [artist['id']for artist in top_artists['items']]
+
+recommendations = sp.recommendations(
+    seed_tracks=seed_tracks[:2], # Use up to 2 seed tracks
+    seed_artists=seed_artists[:2], # Use up to 2 seed artist
+    seed_genres=['pop'],
+    limit=10,
+    target_energy=0.7,
+    target_tempo=120,
+    target_danceability=0.8,
+)
+
+print("Recommended Tracks: ")
+for i, track in enumerate(recommendations['tracks']):
+    print(f"{i + 1}. {track['name']} by {', '.join(artist['name'] for artist in track['artist'])}")
